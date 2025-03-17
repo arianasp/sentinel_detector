0c427dc3bfcf21b65bcaf31b4d2b1957e2aa5d9e
Ari Strandburg-Peshkin <astrandburg@ab.mpg.de>
2025-03-17 13:11:26 +0100

    added reality check

--- read_in_behav_data.R
+++ read_in_behav_data.R
@@ -19,6 +19,8 @@ outfile <- '/mnt/EAS_shared/meerkat/working/processed/behavior/ZU2021_ACC_behavs
 
 #PREPROCESS
 
+reality_check_plot <- F
+
 #put GPS data and ids table into standard format
 ids <- ZU2021_indInfo
 ids$id_code <- ids$code
@@ -31,3 +33,36 @@ behavs <- out$behavs
 behavs_key <- out$behavs_key
 
 save(list = c('behavs','behavs_key'), file = outfile)
+
+
+#reality check - compare with GPS speed for each behavior
+if(reality_check_plot){
+  xs <- ZU2021_allX
+  ys <- ZU2021_allY
+  n_inds <- nrow(xs)
+  n_times <- ncol(xs)
+  
+  speeds <- matrix(NA, nrow = n_inds, ncol = n_times)
+  for(i in 1:n_inds){
+    out <- cocomo::get_heading_and_speed_temporal(xs[i,], ys[i,], t_window = 5, seconds_per_time_step = 1)
+    speeds[i,] <- out$speeds
+  }
+  
+  bins <- seq(-4,2,.4)
+  mids <- bins[2:length(bins)] - diff(bins)/2
+  
+  speeds_hist <- matrix(nrow = 3, ncol = length(mids))
+  for(i in 1:3){
+    speeds_hist[i,] <- hist(log(speeds[which(behavs==i)]),breaks=bins, plot=F)$counts
+    speeds_hist[i,] <- speeds_hist[i,] / sum(speeds_hist[i,])
+  }
+  
+  
+  plot(mids, speeds_hist[1,], xlim = c(-2.5,2), type = 'l', col = 'darkblue', xlab = 'Log speed (m/s)', ylab = 'Probability')
+  cols <- c('darkblue','darkorange','darkred')
+  for(i in 1:3){
+    lines(mids, speeds_hist[i,], col = cols[i],lwd=2)
+  }
+  
+  legend('topright',legend=c('Vigilance','Foraging','Running'),col=cols, lwd=2)
+}
